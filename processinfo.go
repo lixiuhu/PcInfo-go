54f75dd9f11416e0f4427312b694f01ca5d59f81
huyan <lixiuhu_lee@163.com>
2018-03-23 20:39:20 +0800

    update gitignore

---
+++ processinfo.go
@@ -0,0 +1,37 @@
+package processinfo
+
+import (
+	"github.com/mitchellh/go-ps"
+)
+
+func GetProcessPid(ProcessName string) (ProcessId int) {
+	p, _ := ps.Processes()
+	var length int = len(p)
+	var flag int
+	for flag < length {
+		if p[flag].Executable() == ProcessName {
+			return p[flag].Pid()
+		} else {
+			flag++
+		}
+	}
+	return 0
+}
+
+func GetProcessStatus(ProcessName string) (status bool) {
+	p, _ := ps.Processes()
+	var length int = len(p)
+	var flag int = 0
+	for flag < length {
+		if p[flag].Executable() == ProcessName {
+			return true
+		} else {
+			flag++
+		}
+	}
+	return false
+}
+
+// func main() {
+// 	fmt.Println(GetProcessStatus("netbiosd"))
+// }
